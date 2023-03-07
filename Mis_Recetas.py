30c0893ceb55ef6a0e78cea723a04a163239934c
Alexander Velarde <alexandervelarde2511@gmail.com>
2023-03-07 10:48:21 -0400

    Creamos la estrucutra principal de nuestro recetario

---
+++ Mis_Recetas.py
@@ -0,0 +1,39 @@
+#mostrar menu inicio
+
+menu = 0
+
+if menu == 1:
+    #mostrar categorias
+    #elegir categoria
+    #mostrar receeta
+    #elegir  receta
+    #leer receta
+    #volver inicio
+    pass
+elif menu == 2:
+    # mostrar categorias
+    # elegir categoria
+    #crear receta
+    #volver inicio
+    pass
+elif menu == 3:
+    #crear categoria
+    #volver al inicio
+    pass
+elif menu == 4:
+    # mostrar categorias
+    # elegir categoria
+    # mostrar receeta
+    # elegir  receta
+    # eliminar receta
+    # volver inicio
+    pass
+elif menu == 5:
+    # mostrar categorias
+    # elegir categoria
+    #eliminar categorias
+    #volver inicio
+    pass
+elif menu == 6:
+    #finalizar programa
+    pass
